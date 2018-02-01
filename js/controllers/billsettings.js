f468816140f01c31066929d08e5fc048838d90a8
Abhijith C S <abhijithcs1993@gmail.com>
2018-02-02 02:01:22 +0530

    UI pages added

---
+++ js/controllers/billsettings.js
@@ -0,0 +1,12 @@
+
+function openNewBill(){
+	
+	document.getElementById("newBillArea").style.display = "block";
+	document.getElementById("openNewBillButton").style.display = "none";
+}
+
+function hideNewBill(){
+	
+	document.getElementById("newBillArea").style.display = "none";
+	document.getElementById("openNewBillButton").style.display = "block";
+}
